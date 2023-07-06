4dd24b4aad85c331f5ee6e9124318993d4606662
Adrian Zavala <zavaladrian@gmail.com>
2023-07-05 20:26:52 -0500

    Router Setup

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -4,6 +4,12 @@ export const selectAllCampsites = () => {
     return CAMPSITES;
 };
 
-export const selectRandomCampsite = () => {
-    return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
+
+export const selectCampsiteById = (id) => {
+    return CAMPSITES.find((campsite)=> campsite.id == id);
 };
+
+//randomizer
+// export const selectRandomCampsite = () => {
+//     return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
+// };
