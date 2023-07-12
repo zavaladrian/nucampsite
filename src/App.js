b9fd04581807342c76e897ebecde06abe9166455
Adrian Zavala <zavaladrian@gmail.com>
2023-07-12 10:39:52 -0500

    week 3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import { Routes, Route } from 'react-router-dom';
-import CampsiteDetailPage from './pages/CampsiteDetailPage';
-import ContactPage from './pages/ContactPage';
-import HomePage from './pages/HomePage';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import CampsiteDetailPage from "./pages/CampsiteDetailPage";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
+import AboutPage from "./pages/AboutPage";
+import "./App.css";
 
 function App() {
-    return (
-        <div className='App'>
-            <Header />
-                <Routes>
-                    <Route path='/' element={<HomePage />} />
-                    <Route path='contact' element={<ContactPage />} />
-                    <Route path='directory' element={<CampsitesDirectoryPage />} />
-                    <Route path='directory/:campsiteId' element={<CampsiteDetailPage />}
-                    />
-                </Routes>
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="directory" element={<CampsitesDirectoryPage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="directory/:campsiteId" element={<CampsiteDetailPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,22 +1,27 @@
 import { Col, Row } from "reactstrap";
-import DisplayCard from './DisplayCard';
+import DisplayCard from "./DisplayCard";
 import { selectFeaturedCampsite } from "../campsites/campsitesSlice";
 import { selectedFeaturedPromotion } from "../promotion/promotionsSlice";
+import { selectFeaturedPartners } from "../partners/partnersSlice";
 
 const DisplayList = () => {
-    const items =[selectFeaturedCampsite(), selectedFeaturedPromotion()];
+  const items = [
+    selectFeaturedCampsite(),
+    selectedFeaturedPromotion(),
+    selectFeaturedPartners(),
+  ];
 
-    return (
-        <Row>
-            {items.map((item, idx) => {
-                return (
-                    <Col md className='m-1' key={idx}>
-                        <DisplayCard item={item} />
-                    </Col>
-                )
-            })}
-        </Row>
-    );
+  return (
+    <Row>
+      {items.map((item, idx) => {
+        return (
+          <Col md className="m-1" key={idx}>
+            <DisplayCard item={item} />
+          </Col>
+        );
+      })}
+    </Row>
+  );
 };
 
 export default DisplayList;
