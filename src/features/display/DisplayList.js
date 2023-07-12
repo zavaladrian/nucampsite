c41863130d5efc66e455e112e3d2d3fb606185c7
Adrian Zavala <zavaladrian@gmail.com>
2023-07-12 14:17:48 -0500

    ContactPage

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,8 +1,10 @@
 import { Col, Row } from "reactstrap";
-import DisplayCard from "./DisplayCard";
+// import DisplayCard from "./DisplayCard";
+import AnimatedDisplayCard from "./AnimatedDisplayCard";
 import { selectFeaturedCampsite } from "../campsites/campsitesSlice";
 import { selectedFeaturedPromotion } from "../promotion/promotionsSlice";
 import { selectFeaturedPartners } from "../partners/partnersSlice";
+
 
 const DisplayList = () => {
   const items = [
@@ -15,9 +17,11 @@ const DisplayList = () => {
     <Row>
       {items.map((item, idx) => {
         return (
+          item && (
           <Col md className="m-1" key={idx}>
-            <DisplayCard item={item} />
+            <AnimatedDisplayCard item={item} />
           </Col>
+          )
         );
       })}
     </Row>
