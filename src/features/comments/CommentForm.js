a6b51b0fb6bbbdf4bcffadb389e2108085ddf09f
Adrian Zavala <zavaladrian@gmail.com>
2023-07-19 18:22:53 -0500

    addComment

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -9,17 +9,23 @@ import {
 } from "reactstrap";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import { validateCommentForm } from "../../utils/validateCommentForm";
+import { useDispatch } from "react-redux";
+import { addComment } from "./commentsSlice";
+
 
 const CommentForm = ({ campsiteId }) => {
   const [modalOpen, setModalOpen] = useState(false);
+  const dispatch = useDispatch();
   const handleSubmit = (values) => {
     const comment = {
       campsiteId: parseInt(campsiteId),
       rating: values.rating,
       author: values.author,
       text: values.commentText,
-    };
+      date: new Date(Date.now()).toISOString()
+  };
     console.log(comment);
+    dispatch(addComment(comment));
     setModalOpen(false);
   };
 
